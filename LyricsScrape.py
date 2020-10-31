db1508132cb587ecf6b8b6742a314811eab3323b
Dimitar Atanasov <dimitar.a94@gmail.com>
2020-10-31 15:04:34 +0000

    Manually cleaned lyrics files

--- LyricsScrape.py
+++ LyricsScrape.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 23 16:03:28 2016
+Created on Oct 31, 2020
 
 @author: Dimitar Atanasov
 """
@@ -14,46 +14,60 @@ URLs = []
 yearsAll = []
 yearsDict = {}
 
-
 #Get list of links to each song
-for i in range(7):
-    anURL = 'http://www.metrolyrics.com/kanye-west-alpage-'+ str(i+1) +'.html'
-    r = requests.get(anURL)
+
+for i in range(1, 8):
+    #This specific website has 7 pages of songs, so we open them 1 by 1
+    URL = 'http://www.metrolyrics.com/kanye-west-alpage-{}.html'.format(str(i))
+    r = requests.get(URL)
+
+    #After we open the page, extract its html
     rtxt = r.text
     soup = BeautifulSoup(rtxt, "html.parser")
-    txt = soup.get_text()
-    links = soup.find_all('a',{'onmousedown':re.compile(r".*Popular Songs.*")})
-    yearsRaw = soup.find_all('td',{'content':re.compile(r'\d{0,}')})
+
+    #Get all the release years for each song
+    yearsRaw = soup.find_all('td', {'content': re.compile(r'\d{0,}')})
     years = [x.get('content') for x in yearsRaw]
     yearsAll += years
+
+    #Get all 'a' tags in the page containing a URL except for the "Popular Songs" section to avoid duplicates
+    #Then add the URLs to a list
+    links = soup.find_all('a', {'onmousedown': re.compile(r".*Popular Songs.*")})
     hRefs = [x.get('href') for x in links]
     urlsTemp = [x for x in hRefs if x is not None and x.lower().startswith('http:')]
     URLs += urlsTemp
  
-forbiddenChars = ['?','*','>','<','|', ':','\\','/','\"',]
+#Extract lyrics for each song and write them into a txt file
+forbiddenChars = ['?', '*', '>', '<', '|', ':', '\\', '/', '\"']
 
-#Extract lyrics for each song and write them into txt file
 for i in range(len(URLs)):
+    #Open each URL and get the page contents
     dd = requests.get(URLs[i])
     ddtext = dd.text
     x = BeautifulSoup(ddtext, "html.parser")
-    txt = x.get_text()
+
+    #Find song name and remove forbidden characters from it
     songTitle = x.select('title')[0].get_text()
-    songName1 = songTitle[13:-21]
+    songName = songTitle[13:-21]
     for char in forbiddenChars:
-        if  char in songName1:
-            songName = songName1.replace(char, '')
-            songName1 = songName
-    lyricdatatemp = x.find_all('p', {'class':'verse'})
-    lyriclist = [lyricdatatemp[i].get_text() for i in range(len(lyricdatatemp))]
-    lyricdata = '\n'.join(lyriclist)
-    if not os.path.exists(os.path.join('Lyrics2', str(yearsAll[i]))):
-        os.makedirs(os.path.join('Lyrics2', str(yearsAll[i])))
-    text_file = open(os.path.join('Lyrics2', str(yearsAll[i]), songName1 + '.txt'), 'w')
+        if char in songName:
+            songNameTemp = songName.replace(char, '')
+            songName = songNameTemp
+
+    #Extract all verses for each song
+    lyricDataTemp = x.find_all('p', {'class':'verse'})
+    lyricList = [lyricDataTemp[i].get_text() for i in range(len(lyricDataTemp))]
+    lyricData = '\n'.join(lyricList)
+
+    #Write each song to a txt file
+    if not os.path.exists(os.path.join('Lyrics', str(yearsAll[i]))):
+        os.makedirs(os.path.join('Lyrics', str(yearsAll[i])))
+    text_file = open(os.path.join('Lyrics', str(yearsAll[i]), songName + '.txt'), 'w')
     try:    
-        text_file.write(lyricdata)
+        text_file.write(lyricData)
     except:
-        pass
-    text_file.close()
+        continue
+    finally:
+        text_file.close()
  
  
